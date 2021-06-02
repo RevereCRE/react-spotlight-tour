bdba6cbd93e7e08395934ffa11e75322e9b45abc
Ryan Delaney <ryan@reverecre.com>
2021-06-02 14:52:42 -0700

    Add files from downstream

---
+++ src/index.tsx
@@ -0,0 +1,90 @@
+import React, {
+  ComponentType,
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+import type { TutorialConfig, TutorialProps } from './tutorial';
+
+interface AutoTutorialContextValue {
+  configs: TutorialConfig[];
+  addConfig: (config: TutorialConfig) => void;
+  removeConfig: (config: TutorialConfig) => void;
+}
+
+const AutoTutorialContext = createContext<AutoTutorialContextValue>({
+  configs: [],
+  addConfig: () => {},
+  removeConfig: () => {},
+});
+
+export function useAutoTutorial(
+  text: string,
+  placement?: 'bottom' | 'left' | 'right' | 'top'
+) {
+  const [ref, setRef] = useState<HTMLElement | null>(null);
+  const { addConfig, removeConfig } = useContext(AutoTutorialContext);
+
+  useEffect(() => {
+    if (ref == null) return;
+
+    const config: TutorialConfig = { el: ref, text, placement };
+    addConfig(config);
+    return () => {
+      removeConfig(config);
+    };
+  }, [addConfig, removeConfig, ref, text, placement]);
+
+  return setRef;
+}
+
+interface AutoTutorialProps {
+  name: string;
+  seenTutorials: null | readonly (string | null)[];
+  markSeen: (tutorial: string) => void;
+  Tutorial?: ComponentType<TutorialProps>;
+}
+
+export function AutoTutorial({
+  name,
+  seenTutorials,
+  markSeen,
+  Tutorial,
+  children,
+}: PropsWithChildren<AutoTutorialProps>) {
+  const closeTutorial = useCallback(() => {
+    markSeen(name);
+  }, [markSeen, name]);
+
+  const [configs, setConfigs] = useState<TutorialConfig[]>([]);
+
+  const addConfig = useCallback((config: TutorialConfig) => {
+    setConfigs((prevConfigs) => [...prevConfigs, config]);
+  }, []);
+
+  const removeConfig = useCallback((config: TutorialConfig) => {
+    setConfigs((prevConfigs) => prevConfigs.filter((c) => c !== config));
+  }, []);
+
+  const autoTutorialContextValue = useMemo(
+    () => ({
+      configs,
+      addConfig,
+      removeConfig,
+    }),
+    [configs, addConfig, removeConfig]
+  );
+
+  return (
+    <AutoTutorialContext.Provider value={autoTutorialContextValue}>
+      {children}
+      {!(seenTutorials ?? []).includes(name) && Tutorial != null && (
+        <Tutorial conf={configs} onClick={closeTutorial} />
+      )}
+    </AutoTutorialContext.Provider>
+  );
+}
---
+++ src/tutorial.tsx
@@ -0,0 +1,288 @@
+import React, {
+  memo,
+  PropsWithChildren,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
+import { createPortal } from 'react-dom';
+
+function checkExhaustive(cased: never): asserts cased is never {
+  return undefined;
+}
+
+function TutorialPortal({ children }: PropsWithChildren<unknown>) {
+  const outlet = useRef<HTMLElement | null>(null);
+  const [didMount, setDidMount] = useState(false);
+  useEffect(() => {
+    let outletDiv = document.getElementById('tutorial-outlet');
+    if (!outletDiv) {
+      outletDiv = document.createElement('div');
+      outletDiv.id = 'tutorial-outlet';
+      document.body.appendChild(outletDiv);
+    }
+
+    outlet.current = outletDiv;
+    setDidMount(true);
+  }, [outlet]);
+
+  if (!didMount) return null;
+  return createPortal(didMount ? children : null, outlet.current!);
+}
+
+export interface TutorialConfig {
+  el: HTMLElement | null;
+  text: string;
+  placement?: 'bottom' | 'left' | 'right' | 'top';
+}
+
+// Controls overall size of the tutorial.
+const UNIT = 12;
+const UNIT_2 = UNIT * 2;
+const UNIT_3 = UNIT * 3;
+const UNIT_4 = UNIT * 4;
+const UNIT_5 = UNIT * 5;
+
+function renderTextBottom(
+  ctx: CanvasRenderingContext2D,
+  elRect: DOMRect,
+  textParts: string[]
+) {
+  ctx.fillRect(
+    elRect.x - UNIT,
+    elRect.y + elRect.height + UNIT_2,
+    elRect.width + UNIT_2,
+    2
+  );
+  ctx.fillRect(
+    elRect.x + Math.floor(elRect.width / 2),
+    elRect.y + elRect.height + UNIT_2,
+    2,
+    UNIT_2
+  );
+
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'top';
+  for (let i = 0; i < textParts.length; i++) {
+    ctx.fillText(
+      textParts[i],
+      elRect.x + Math.floor(elRect.width / 2),
+      elRect.y + elRect.height + UNIT_4 + UNIT_3 * i
+    );
+  }
+}
+
+function renderTextTop(
+  ctx: CanvasRenderingContext2D,
+  elRect: DOMRect,
+  textParts: string[]
+) {
+  ctx.fillRect(elRect.x - UNIT, elRect.y - UNIT_2, elRect.width + UNIT_2, 2);
+  ctx.fillRect(
+    elRect.x + Math.floor(elRect.width / 2),
+    elRect.y - UNIT_4,
+    2,
+    UNIT_2
+  );
+
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'bottom';
+  for (let i = 0; i < textParts.length; i++) {
+    ctx.fillText(
+      textParts[i],
+      elRect.x + Math.floor(elRect.width / 2),
+      elRect.y - UNIT_5 - UNIT_3 * (textParts.length - i - 1)
+    );
+  }
+}
+
+function renderTextLeft(
+  ctx: CanvasRenderingContext2D,
+  elRect: DOMRect,
+  textParts: string[]
+) {
+  ctx.fillRect(elRect.x - UNIT_2, elRect.y - UNIT, 2, elRect.height + UNIT_2);
+  ctx.fillRect(
+    elRect.x - UNIT_4,
+    elRect.y + Math.floor(elRect.height / 2),
+    UNIT_2,
+    2
+  );
+  for (let i = 0; i < textParts.length; i++) {
+    ctx.textAlign = 'right';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(
+      textParts[i],
+      elRect.x - UNIT_5,
+      elRect.y + Math.floor(elRect.height / 2) + UNIT_3 * i
+    );
+  }
+}
+function renderTextRight(
+  ctx: CanvasRenderingContext2D,
+  elRect: DOMRect,
+  textParts: string[]
+) {
+  ctx.fillRect(
+    elRect.x + elRect.width + UNIT_2,
+    elRect.y - UNIT,
+    2,
+    elRect.height + UNIT_2
+  );
+  ctx.fillRect(
+    elRect.x + elRect.width + UNIT_2,
+    elRect.y + Math.floor(elRect.height / 2),
+    UNIT_2,
+    2
+  );
+  for (let i = 0; i < textParts.length; i++) {
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(
+      textParts[i],
+      elRect.x + elRect.width + UNIT_5,
+      elRect.y + Math.floor(elRect.height / 2) + UNIT_3 * i
+    );
+  }
+}
+
+function renderConfig(
+  ctx: CanvasRenderingContext2D,
+  { el, text, placement = 'bottom' }: TutorialConfig
+) {
+  if (el == null) return;
+
+  const elRect = el.getBoundingClientRect();
+  ctx.clearRect(
+    elRect.x - UNIT,
+    elRect.y - UNIT,
+    elRect.width + UNIT_2,
+    elRect.height + UNIT_2
+  );
+
+  ctx.fillStyle = 'white';
+
+  ctx.font = `${UNIT_3}px sans-serif`;
+  const textParts = text.split('\n');
+
+  switch (placement) {
+    case 'bottom':
+      renderTextBottom(ctx, elRect, textParts);
+      break;
+    case 'top':
+      renderTextTop(ctx, elRect, textParts);
+      break;
+    case 'left':
+      renderTextLeft(ctx, elRect, textParts);
+      break;
+    case 'right':
+      renderTextRight(ctx, elRect, textParts);
+      break;
+    default:
+      checkExhaustive(placement);
+  }
+}
+
+function getCanvasContext(canvas: HTMLCanvasElement) {
+  const pixelRatio = window.devicePixelRatio ?? 1;
+
+  const height = Math.min(document.body.clientHeight, window.innerHeight * 2);
+  const width = Math.min(document.body.clientWidth, window.innerWidth * 2);
+
+  canvas.height = height * pixelRatio;
+  canvas.width = width * pixelRatio;
+  canvas.style.height = `${height}px`;
+  canvas.style.width = `${width}px`;
+
+  const canvasRect = canvas.getBoundingClientRect();
+  const ctx = canvas.getContext('2d');
+
+  if (ctx == null) return null;
+
+  ctx.scale(pixelRatio, pixelRatio);
+  ctx.fillStyle = 'black';
+
+  ctx.globalAlpha = 0.7;
+  ctx.fillRect(0, 0, canvasRect.width, canvasRect.height);
+  ctx.globalAlpha = 1;
+
+  return ctx;
+}
+
+export interface TutorialProps {
+  conf: TutorialConfig[];
+  onClick?: () => void;
+}
+
+function Tutorial({ conf, onClick }: TutorialProps) {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
+
+  useLayoutEffect(() => {
+    if (canvas == null) {
+      return;
+    }
+
+    function drawTutorial() {
+      if (containerRef.current != null) {
+        containerRef.current.style.height = `${document.body.clientHeight}px`;
+        containerRef.current.style.width = `${document.body.clientWidth}px`;
+      }
+
+      const ctx = getCanvasContext(canvas!);
+      if (ctx == null) return;
+
+      for (const tutorialConfig of conf) {
+        renderConfig(ctx, tutorialConfig);
+      }
+    }
+
+    const resizeObserver = new ResizeObserver(drawTutorial);
+    resizeObserver.observe(document.body);
+    for (const { el } of conf) {
+      if (el) resizeObserver.observe(el);
+    }
+
+    const drawInitialFrame = requestAnimationFrame(drawTutorial);
+    return () => {
+      cancelAnimationFrame(drawInitialFrame);
+      resizeObserver.disconnect();
+    };
+  }, [canvas, conf]);
+
+  const hasEls = conf.some(({ el }) => el != null);
+  if (!hasEls) return null;
+
+  return (
+    <TutorialPortal>
+      <div
+        ref={containerRef}
+        onClick={onClick}
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          zIndex: 10,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <canvas ref={setCanvas} />
+        <div style={{ backgroundColor: 'black', opacity: 0.7, flex: 1 }} />
+      </div>
+    </TutorialPortal>
+  );
+}
+
+const TutorialMemo = memo(Tutorial, (prev, next) => {
+  if (prev.conf.length !== next.conf.length) return false;
+  return prev.conf.every(
+    (el, i) =>
+      el.el === next.conf[i].el &&
+      el.text === next.conf[i].text &&
+      el.placement === next.conf[i].placement
+  );
+});
+
+export { TutorialMemo as Tutorial };
